e6f97f77cadd66cdd9609cdf891936fd42be3231
Chadeya-MCR <chadeyamorgan203@gmail.com>
2023-12-22 06:05:20 +0300

    Add prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
